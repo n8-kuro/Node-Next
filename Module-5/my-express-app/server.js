873c2b449879e146fc7e265a9fafda8609f0f54a
n8-kuro <yuushamail.com>
2026-04-28 17:01:14 +0600

    update server.js and package.json

--- Module-5/my-express-app/server.js
+++ Module-5/my-express-app/server.js
@@ -6,7 +6,16 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello, express' });
+    res.json({ message: 'Home page' });
+});
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    res.json({ message: 'Запроше пользователь с ID: ${userId}'});
+});
+
+app.post('/users', (req, res) => {
+    const { name, email } = req.body;
+    res.status(201).json({ message: 'Пользователь создан', name, email});
 });
 
 app.listen(PORT, () => {
